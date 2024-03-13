bb251388d43b4c8232d5804fc9c6e278d03a7525
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-03-13 01:14:59 -0600

    Update index.js with database connection

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql'
 import schema from "./schema.js"
+import { connect } from './database.js';
 
 const app = express();
+connect();
 
 app.get('/', (req, res) => {
     res.json({ message: 'Hello World' });
 });
 
-
-
 app.use('/graphql', graphqlHTTP({
     graphiql: true,
-    schema: schema
+    schema: schema,
+    context: {
+        messageId: 'test'
+    }
 }))
 
 app.listen(3000, () => console.log('Server is running on port 3000'));
