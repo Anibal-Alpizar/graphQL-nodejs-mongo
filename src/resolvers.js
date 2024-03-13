c4dbb693fc1b9d87cc927d7ed327d2625bfb341d
anibal-alpizar <anibal.alpizar14@gmail.com>
2024-03-13 01:15:30 -0600

    Add CRUD operations for User model using graphql

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,15 +1,20 @@
 import { tasks } from './sample.js'
+import User from './models/User.js'
 
 export const resolvers = {
     Query: {
         hello: () => {
             return 'Hello World'
         },
-        greet(root, { name }) {
+        greet(root, { name }, ctx) {
+            console.log(ctx)
             return `Hello, ${name}`
         },
         tasks() {
             return tasks
+        },
+        async users() {
+            return await User.find()
         }
     },
 
@@ -18,6 +23,16 @@ export const resolvers = {
             input._id = tasks.length
             tasks.push(input)
             return input
+        },
+        async createUser(_, { input }) {
+            const newUser = new User(input)
+            return await newUser.save()
+        },
+        async deleteUser(_, { _id }) {
+            return await User.findByIdAndDelete(_id);
+        },
+        async updateUser(_, { _id, input }) {
+            return await User.findByIdAndUpdate(_id, input, { new: true });
         }
     }
 }
--- src/schema.js
+++ src/schema.js
@@ -2,11 +2,11 @@ import { makeExecutableSchema } from "graphql-tools"
 import { resolvers } from "./resolvers.js"
 
 const typeDefs = `
-
     type Query {    
         hello: String
         greet(name: String!): String
         tasks: [Task]
+        users: [User]
     }
 
     type Task {
@@ -16,16 +16,30 @@ const typeDefs = `
         number: Int
     }
 
+    type User {
+        _id: ID
+        firstname: String!
+        lastname: String!
+        age: Int!
+    }
+
     type Mutation {
         createTask(input: TaskInput): Task
-        updateTask(_id: ID, input: TaskInput): Task
-        deleteTask(_id: ID): Task
+        createUser(input: UserInput): User
+        deleteUser(_id: ID): User
+        updateUser(_id: ID, input: UserInput): User
     }
 
     input TaskInput {
         title: String!
         description: String!
         number: Int
+    }
+
+    input UserInput {
+        firstname: String!
+        lastname: String!
+        age: Int
     }
 `;
 
